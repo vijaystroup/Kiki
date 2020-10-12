50d7fec9ccab012f5b6b8dbafd138a559951018a
Vijay Stroup <vijaystroup@gmail.com>
2020-10-12 13:05:53 -0400

    filename option

--- kiki/kiki.py
+++ kiki/kiki.py
@@ -2,15 +2,16 @@ import typer
 
 
 def main(
-    name: str = typer.Argument(...)
+    pdf: str = typer.Argument(..., help='url or file of pdf'),
+    filename: str = typer.Option('pdf', help='name of output file without extension')
 ):
     """
     Kiki\n
     Example Documentation
     """
 
-    typer.echo(f"Hello {name}")
+    typer.echo(f'PDF: {pdf}\tFILENAME: {filename}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     typer.run(main)
