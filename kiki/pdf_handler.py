af6bdfac6a13ea8068da628dae55c79d41a57eba
Vijay Stroup <vijaystroup@gmail.com>
2020-10-13 18:49:15 -0400

    download tts

--- kiki/pdf_handler.py
+++ kiki/pdf_handler.py
@@ -1,13 +1,18 @@
 from PyPDF2 import PdfFileReader
 from typer import progressbar
+from gtts import gTTS
 
 
 def file_pdf(file_name: str):
     pdf = PdfFileReader(file_name, strict=False)
 
     with progressbar(length=pdf.getNumPages(), label='Downloading') as progress:
-        for page in progress:
-            text = pdf.getPage(page).extractText()
+        with open('asdf.mp3', 'wb') as f:
+            for page in progress:
+                text = pdf.getPage(page).extractText()
+                if text:
+                    tts = gTTS(text)
+                    tts.write_to_fp(f)
 
 
-file_pdf('/mnt/c/Users/Vijay/Downloads/thermalphysics-2ed-Blundell.pdf')
+file_pdf('/mnt/c/Users/Vijay/Downloads/CitationNeededBook-Sample.pdf')
