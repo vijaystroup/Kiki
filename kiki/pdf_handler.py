cb269c813b5a0e62c19562cb9ceaae60c883df97
Vijay Stroup <vijaystroup@gmail.com>
2020-10-14 11:02:38 -0400

    download function to be used by file or url pdfs

--- kiki/pdf_handler.py
+++ kiki/pdf_handler.py
@@ -1,11 +1,9 @@
+from gtts import gTTS
 from PyPDF2 import PdfFileReader
 from typer import progressbar
-from gtts import gTTS
 
 
-def file_pdf(file_name: str):
-    pdf = PdfFileReader(file_name, strict=False)
-
+def download(pdf):
     with progressbar(length=pdf.getNumPages(), label='Downloading') as progress:
         with open('asdf.mp3', 'wb') as f:
             for page in progress:
@@ -13,6 +11,16 @@ def file_pdf(file_name: str):
                 if text:
                     tts = gTTS(text)
                     tts.write_to_fp(f)
+
+
+def file_pdf(file_name):
+    pdf = PdfFileReader(file_name, strict=False)
+    download(pdf)
+
+
+def url_pdf(url):
+    # make PdfFileReader obj out of beautiful soup
+    pass
 
 
 file_pdf('/mnt/c/Users/Vijay/Downloads/CitationNeededBook-Sample.pdf')
