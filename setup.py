0aa70ce0571e57fd22c1888a9b68876945a4f390
Vijay Stroup <vijaystroup@gmail.com>
2020-10-14 16:06:30 -0400

    long description

--- setup.py
+++ setup.py
@@ -7,12 +7,20 @@ def get_requirements():
         return data.split('\n')
 
 
+def get_long_desc():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
 setup(
     name = 'kiki',
     version = '1.0',
     description = 'pdf text-to-speech',
-    author = 'Vijay Stroup',
+    long_description = get_long_desc(),
+    long_description_content_type = 'text/markdown',
     url = 'https://github.com/VijayStroup/Kiki',
+    author = 'Vijay Stroup',
+    author_email = 'vijay@vijaystroup.com',
     packages = find_packages(),
     include_package_data = True,
     install_requires = get_requirements(),
